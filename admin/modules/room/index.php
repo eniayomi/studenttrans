d0a87b4282063d8dbe6274ae45555e2e55a319be
eniayomi <oluwaseyieniayomi@gmail.com>
2019-07-03 07:53:13 +0100

    first commit

---
+++ admin/modules/room/index.php
@@ -0,0 +1,31 @@
+<link rel="shortcut icon" type="image/x-icon" href="../../../images/fav.png"/>
+
+<?php
+require_once("../../../includes/initialize.php");
+//checkAdmin();
+$view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
+
+switch ($view) {
+	case 'list' :
+		$content    = 'list.php';		
+		break;
+
+	case 'add' :
+		$content    = 'add.php';		
+		break;
+
+	case 'edit' :
+		$content    = 'edit.php';		
+		break;
+    case 'view' :
+		$content    = 'view.php';		
+		break;
+
+	default :
+		$content    = 'list.php';		
+}
+require_once '../../theme/frontendTemplate.php';
+?>
+
+
+  
---
+++ home.php
@@ -0,0 +1,40 @@
+<?php //include 'banner.php';?>
+<link rel="shortcut icon" type="image/x-icon" href="images/fav.png"/>
+
+<div class="container">
+
+	<div class="col-xs-12 col-sm-9">
+		<div class="rows">
+			<div class="well">
+				<fieldset>
+					<legend><h2 class="text-left">Welcome!</h2></legend>
+					<h4>Student Transcript Processing System</h4>
+				</fieldset>	
+				<br/>
+<br/>
+				
+			</div>
+		</div>
+	</div>
+	<!--/span--> 
+	<div class="row row-offcanvas row-offcanvas-right">
+		<div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+			<div class="sidebar-nav">
+				<div class="panel panel-success">
+				
+			  		<div class="panel-heading">Login Information</div>
+					   <div class="panel-body">	
+							<div class="col-xs-12 col-sm-12">
+							 <span class="glyphicon glyphicon-user"> </span> <label><?Php echo $_SESSION['FNAME'];?></label><br/>
+							 <span class="glyphicon glyphicon-cog"> </span> <label><?Php echo $_SESSION['LNAME'];?></label><br/>
+							  <a href="logout.php" class="btn btn-default">Logout <span class="glyphicon glyphicon-log-out"></span></a>
+							</div>					            					            		
+						</div>
+					          
+				</div>
+			</div>
+		</div>
+	</div>
+<!--/.well --> 
+</div><!--container-->
+	
